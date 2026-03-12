794bc3e348f52b0735fff8c9d41bf52cd4a5553f
rodrigorocha1 <silva.rodrigo31@outlook.com>
2026-03-11 21:09:22 -0300

    feat: completo para a coleta

--- app/src/observador/duckdb_observador.py
+++ app/src/observador/duckdb_observador.py
@@ -21,7 +21,7 @@ class DuckDBObservador(Observador[Stream]):
 
         self.__con.execute(
             """
-            SET s3_endpoint='localhost:9000'; 
+            SET s3_endpoint='minio:9000'; 
             SET s3_access_key_id='minio';
             SET s3_secret_access_key='minio123';
             SET s3_region='us-east-1';
