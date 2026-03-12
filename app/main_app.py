50080c65dcd45621ba50cd8baab94ece7421d673
rodrigorocha1 <silva.rodrigo31@outlook.com>
2026-03-11 21:09:28 -0300

    feat: completo para a coleta

--- app/main_app.py
+++ app/main_app.py
@@ -1,41 +1,70 @@
 import asyncio
 import os
+import signal
+import sys
 from datetime import datetime, timezone
 
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from apscheduler.triggers.interval import IntervalTrigger
 from dotenv import load_dotenv
 from twitchAPI.object.api import Stream
 from twitchAPI.twitch import Twitch
 
 from app.src.assunto.assunto import Assunto
 from app.src.observador.duckdb_observador import DuckDBObservador
+from app.src.utlis.utlis_log import logger
 
 load_dotenv()
+
 CLIENTE_SECRET = os.getenv("CLIENTE_SECRET")
 CLIENTE_ID = os.getenv("CLIENTE_ID")
 
+scheduler = AsyncIOScheduler()
 
-async def main():
+
+async def coletar_streams():
     twitch = Twitch(CLIENTE_ID, CLIENTE_SECRET)
-
     await twitch.authenticate_app([])
-
     assunto = Assunto[Stream]()
     duckdb_observador = DuckDBObservador()
     assunto.anexar_observador(duckdb_observador)
-
-    # data atual em UTC
     today = datetime.now(timezone.utc).date()
-
-    async for stream in twitch.get_streams(language=['pt']):
+    logger.info('Coletando dados streams...')
+    async for stream in twitch.get_streams(language=["pt"]):
         started_at = stream.started_at
-
         if started_at.date() == today:
-            print(
-                stream
-            )
-            print(type(stream))
             assunto.notificar_observador(dados=stream)
-            break
+    await twitch.close()
+    logger.info('Fim coleta dados streams...')
 
 
-asyncio.run(main())
+def desativar_rotina(signum, frame):
+    logger.info("Encerrando scheduler...")
+    scheduler.shutdown()
+    sys.exit(0)
+
+
+async def main():
+    scheduler.add_job(
+        coletar_streams,
+        trigger=IntervalTrigger(minutes=15),
+        id="job_15_min",
+        replace_existing=True,
+        max_instances=1,
+        coalesce=True,
+    )
+
+    scheduler.start()
+
+    logger.info("Scheduler iniciado...")
+
+    # mantém o loop vivo
+    while True:
+        await asyncio.sleep(1)
+
+
+if __name__ == "__main__":
+    signal.signal(signal.SIGTERM, desativar_rotina)
+    signal.signal(signal.SIGINT, desativar_rotina)
+    asyncio.run(coletar_streams())
+    asyncio.run(main())
